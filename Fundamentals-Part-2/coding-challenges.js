cd48aef32897af1fd54cfa52b608042d01c94d6e
José Eduardo Tejeda León <eduardotejedaleon@gmail.com>
2024-08-15 23:14:09 -0600

    formatting console log

--- Fundamentals-Part-2/coding-challenges.js
+++ Fundamentals-Part-2/coding-challenges.js
@@ -236,7 +236,7 @@ TEST DATA 2: [12, 5, -5, 0, 4]
 const printForecast = function (arr) {
   let forecast = "...";
   for (let i = 0; i < arr.length; i++) {
-    forecast += ` ${arr[i]}ºC in ${i + 1} days ...`;
+    forecast += ` ${arr[i]}ºC in ${i + 1} days ... `;
   }
   return forecast;
 };
